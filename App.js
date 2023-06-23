024c19e1568380b54b382db162acbc32731b0ed2
harshittrivedi1234 <117967106+harshittrivedi1234@users.noreply.github.com>
2023-06-23 13:44:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,91 @@
+// import './App.css';
+
+// import React, { Component } from 'react'
+// import Navbar from './components/Navbar';
+// import News from './components/News';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+// import LoadingBar from 'react-top-loading-bar'
+
+// export default class App extends Component {
+//   pageSize=15;
+//   // apiKey= process.env.REACT_APP_NEWS_API
+
+//   state={
+//     progress:0
+//   }
+
+//   setProgress=(progress)=>{
+//     setState({progress:progress})
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <Router>
+//           <Navbar/>
+//           <LoadingBar
+//           height={3}
+//         color='#f11946'
+//         progress={state.progress}
+//       />
+//           <Routes>
+//             <Route path='/' element={<News setProgress={setProgress} key='general' pageSize={pageSize} country='in' category='general' />}></Route>
+//             <Route path='/business' element={<News setProgress={setProgress} key='business' pageSize={pageSize} country='in' category='business' />}></Route>
+//             <Route path='/entertainment' element={<News setProgress={setProgress} key='entertainment' pageSize={pageSize} country='in' category='entertainment' />}></Route>
+//             <Route path='/health' element={<News setProgress={setProgress}  key='health' pageSize={pageSize} country='in' category='health' />}></Route>
+//             <Route path='/science' element={<News setProgress={setProgress}  key='science' pageSize={pageSize} country='in' category='science' />}></Route>
+//             <Route path='/sports' element={<News setProgress={setProgress} key='sports' pageSize={pageSize} country='in' category='sports' />}></Route>
+        
+//           </Routes>
+//         </Router>
+
+//       </div>
+//     )
+//   }
+// }
+
+import './App.css';
+
+import React, { useState} from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import { BrowserRouter as Router, Routes,Route,} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+const App = ()=> {
+  const pageSize=15;
+  // const apiKey= process.env.REACT_APP_NEWS_API
+
+  const [progress, setProgress] = useState(0)
+
+ 
+
+    return (
+      <div>
+        <Router>
+          <Navbar/>
+          <LoadingBar
+          height={3}
+        color='#f11946'
+        progress={progress}
+      />
+          <Routes>
+            <Route path='/general' element={<News setProgress={setProgress} key='general' pageSize={pageSize} country='in' category='general' />}></Route>
+            <Route path='/business' element={<News setProgress={setProgress} key='business' pageSize={pageSize} country='in' category='business' />}></Route>
+            <Route path='/entertainment' element={<News setProgress={setProgress} key='entertainment' pageSize={pageSize} country='in' category='entertainment' />}></Route>
+            <Route path='/health' element={<News setProgress={setProgress}  key='health' pageSize={pageSize} country='in' category='health' />}></Route>
+            <Route path='/science' element={<News setProgress={setProgress}  key='science' pageSize={pageSize} country='in' category='science' />}></Route>
+            <Route path='/sports' element={<News setProgress={setProgress} key='sports' pageSize={pageSize} country='in' category='sports' />}></Route>
+        
+          </Routes>
+        </Router>
+
+      </div>
+    )
+  
+}
+
+export default App
